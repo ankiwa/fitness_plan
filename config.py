c62c74c6c01791395a3e15776a40d8cb207cf4eb
ankiwa <156565689+ankiwa@users.noreply.github.com>
2024-06-08 16:35:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-MODEL_FILE_PATH = r'project_app\Linear_model.pkl'
-JSON_FILE_PATH = r'project_app\project_data.json'
+MODEL_FILE_PATH = r'project_app/Linear_model.pkl'
+JSON_FILE_PATH = r'project_app/project_data.json'
 
 PORT_NUMBER = 8080
 # MODEL_FILE_PATH = 'Linear_Model.pkl'
